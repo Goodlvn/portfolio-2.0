664cf63f30d2c9a75354abbe60b5b87103ee2059
JD Martinez <focus4ursoul@gmail.com>
2020-10-10 21:03:08 -0700

    add modal functionality to portfolio page

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const mobileMenuBtn = document.querySelector(".navbar-burger");
 const mobileMenu = document.querySelector("#mobileMenu");
+const modalBtnCont = document.querySelector(".projects");
+const modalCont = document.querySelector(".modal-container");
+const modals = Array.from(document.getElementsByClassName("modal"));
 
 mobileMenuBtn.addEventListener("click", function () {
 
@@ -12,5 +15,21 @@ mobileMenuBtn.addEventListener("click", function () {
         mobileMenuBtn.classList.add("is-active");
         mobileMenu.classList.add("is-active");
     };
+});
 
+
+modalBtnCont.addEventListener("click", event => {
+    if (event.target.classList.contains("gifBtn")) {
+        modals.forEach(modal => {
+            if(event.target.parentElement.id === modal.getAttribute('data-proj') ){
+                modal.classList.add("is-active");
+            };
+        });
+    };
+});
+
+modalCont.addEventListener("click", event => {
+    if(event.target.className === "modal-background" || event.target.classList.contains("modal-close")){
+        event.target.parentElement.classList.remove("is-active");
+    }
 });
